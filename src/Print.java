af6c4c2c9369a20ce7cb070a19afb7a3bab7b96d
zvidolova <zvidolova@gmail.com>
2017-03-20 14:36:45 +0100

    Initial commit

---
+++ src/Print.java
@@ -0,0 +1,15 @@
+
+public class Print {
+
+	public static void main(String[] args) {
+		int myNumber= 34;
+		short myShortNumber= 1;
+		System.out.println(myShortNumber);
+		String myString ="Hello World";
+		System.out.println(myString);
+		System.out.println(myNumber);
+		System.out.println("Marmi");
+		
+	}
+
+}
